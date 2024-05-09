39a88b8a00330aba2e359d948f278c9d4803e371
Bangladesh <Bangladesh@DESKTOP-KD6BL67>
2024-05-09 18:59:56 +0600

    Add project files.

---
+++ PracticeMAUI.API2/Migrations/20240427034432_TTTT.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PracticeMAUI.API2.Migrations
+{
+    /// <inheritdoc />
+    public partial class TTTT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Employees");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
