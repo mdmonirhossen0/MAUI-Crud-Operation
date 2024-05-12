026a3bede1a1071a5f36caa9132eceb08e27c9c3
Monir <mdmonir.itworld@gmail.com>
2024-05-12 20:43:34 +0600

    first commit

---
+++ PracticeMAUI/obj/Debug/net8.0-android/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,12 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("PracticeMAUI.MainApplication, PracticeMAUI, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc648125dbf141f9f182.MainApplication.class, crc648125dbf141f9f182.MainApplication.__md_methods);
+		mono.android.Runtime.register ("Microsoft.Maui.MauiApplication, Microsoft.Maui, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", crc6488302ad6e9e4df1a.MauiApplication.class, crc6488302ad6e9e4df1a.MauiApplication.__md_methods);
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# MAUI-Crud-Operation" 
